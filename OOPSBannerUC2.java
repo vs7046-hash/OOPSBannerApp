edb1bb60292ae314f0a413277e10e378b67b52a2
vs7046-hash <vs7046@srmist.edu.in>
2026-03-03 09:14:41 +0530

    Create OOPSBannerUC2.java

---
+++ OOPSBannerUC2.java
@@ -0,0 +1,18 @@
+public class OOPSBannerUC2 {
+    public static void main(String[] args) {
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+
+        System.out.println(" *     * " + " *     * " + "  *     *" + " *       ");
+
+        System.out.println(" *     * " + " *     * " + "  *     *" + " *       ");
+
+        System.out.println(" *     * " + " *     * " + "  ****** " + "  *****  ");
+
+        System.out.println(" *     * " + " *     * " + "  *      " + "       * ");
+
+        System.out.println(" *     * " + " *     * " + "  *      " + "       * ");
+
+        System.out.println("  *****  " + "  *****  " + "  *      " + "  ****** ");
+    }
+}
