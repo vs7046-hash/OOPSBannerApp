6ac11c86cd801a897ec76b5a8b67898ff4528765
vs7046-hash <vs7046@srmist.edu.in>
2026-03-03 09:22:23 +0530

    Create BannerAppUC5.java

---
+++ BannerAppUC5.java
@@ -0,0 +1,47 @@
+public class BannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        " ******",
+                        " ******"),
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*     *"),
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        " ******",
+                        " ******"),
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "      *"),
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "*     *"),
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        "*      ",
+                        " ******")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
