5c6788f09a869c9f7c8ba32270f3f41a58e7f1c4
vs7046-hash <vs7046@srmist.edu.in>
2026-03-03 09:24:23 +0530

    Create BannerAppUC3.java

---
+++ BannerAppUC3.java
@@ -0,0 +1,46 @@
+public class OOPSBannerAppUC3 {
+    public static void main(String[] args) {
+ System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                "  ****** "
+        ));
+ System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *      *"
+        ));
+ System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "
+        ));
+ System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  "
+        ));
+ System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * "
+        ));
+ System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *      *"
+        ));
+ System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  ****** "
+        ));
+    }
+}
